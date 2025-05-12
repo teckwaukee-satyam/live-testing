152c02bd9967a386696977415ff656d1e4c42253
techwaukee-satyam <satyam.kumar@techwaukee.com>
2025-05-12 16:43:58 +0530

    home page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
+// Make sure this matches exactly
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace live_testing.Pages
+namespace DesignAgency.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
-
         }
     }
 }
